ab783d829721a755f3dd719ab50fa547c75b65fd
noob58382 <170640893+noob58382@users.noreply.github.com>
2026-09-20 23:42:32 +0800

    Add configuration for products and stores

---
+++ src/config.js
@@ -0,0 +1,39 @@
+export const PRODUCTS = [
+  {
+    name: "iPhone 18 Pro 512GB 冰川色",
+    partNumber: "MJRX4ZA/A",
+    productUrl: "https://www.apple.com/hk/shop/buy-iphone"
+  }
+];
+
+export const STORES = [
+  {
+    id: "R409",
+    name: "Apple Causeway Bay"
+  },
+  {
+    id: "R428",
+    name: "Apple ifc mall"
+  },
+  {
+    id: "R485",
+    name: "Apple Festival Walk"
+  },
+  {
+    id: "R499",
+    name: "Apple Canton Road"
+  },
+  {
+    id: "R610",
+    name: "Apple New Town Plaza"
+  },
+  {
+    id: "R673",
+    name: "Apple apm Hong Kong"
+  }
+];
+
+export const APPLE_API =
+  "https://www.apple.com/hk-zh/shop/retail/pickup-message";
+
+export const POLL_DELAY_MS = 500;
